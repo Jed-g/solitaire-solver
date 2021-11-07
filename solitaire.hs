c9dfc61388f19c0e3078626692b7532345a6475b
Jed G <jed.golebiewski@gmail.com>
2021-11-07 21:21:23 +0000

    Do step 4

--- solitaire.hs
+++ solitaire.hs
@@ -37,15 +37,45 @@ type Reserve = [Card]
 data Board = EOBoard Foundations Columns Reserve
 
 appendixABoard :: Board
-appendixABoard = EOBoard [] [[(Four, Spades), (King, Clubs), (Queen, Hearts), (Ace, Hearts), (Seven, Diamonds), (Ace, Clubs)],
-    [(Seven, Hearts), (Six, Spades), (Five, Spades), (Three, Diamonds), (Queen, Diamonds), (Five, Diamonds)],
-    [(Eight, Diamonds), (Five, Hearts), (Queen, Diamonds), (Seven, Spades), (Ten, Diamonds), (King, Hearts)],
-    [(Queen, Spades), (Ten, Clubs), (Eight, Spades), (Seven, Clubs), (Six, Hearts), (Jack, Spades)],
-    [(Four, Clubs), (Jack, Hearts), (King, Diamonds), (Ace, Diamonds), (Eight, Clubs), (Ace, Spades)],
-    [(Jack, Clubs), (Six, Diamonds), (Ten, Hearts), (Three, Clubs), (Three, Hearts), (Two, Diamonds)],
-    [(Ten, Spades), (Three, Spades), (Nine, Hearts), (Nine, Clubs), (Four, Diamonds), (Nine, Spades)],
-    [(Eight, Hearts), (King, Spades), (Nine, Diamonds), (Four, Hearts), (Two, Spades), (Two, Clubs)]] [(Two, Hearts), (Six, Clubs), (Five, Clubs), (Jack, Diamonds)]
+appendixABoard = EOBoard [] [[(Ace, Clubs), (Seven, Diamonds), (Ace, Hearts), (Queen, Hearts), (King, Clubs), (Four, Spades)],
+        [(Five, Diamonds), (Queen, Diamonds), (Three, Diamonds), (Five, Spades), (Six, Spades), (Seven, Hearts)],
+        [(King, Hearts), (Ten, Diamonds), (Seven, Spades), (Queen, Diamonds), (Five, Hearts), (Eight, Diamonds)],
+        [(Jack, Spades), (Six, Hearts), (Seven, Clubs), (Eight, Spades), (Ten, Clubs), (Queen, Spades)],
+        [(Ace, Spades), (Eight, Clubs), (Ace, Diamonds), (King, Diamonds), (Jack, Hearts), (Four, Clubs)],
+        [(Two, Diamonds), (Three, Hearts), (Three, Clubs), (Ten, Hearts), (Six, Diamonds), (Jack, Clubs)],
+        [(Nine, Spades), (Four, Diamonds), (Nine, Clubs), (Nine, Hearts), (Three, Spades), (Ten, Spades)],
+        [(Two, Clubs), (Two, Spades), (Four, Hearts), (Nine, Diamonds), (King, Spades), (Eight, Hearts)]] [(Two, Hearts), (Six, Clubs), (Five, Clubs), (Jack, Diamonds)]
 
 instance Show Board where
     show (EOBoard foundations columns reserve) = "EOBoard\nFoundations " ++ show foundations ++ "\nColumns\n" ++
         concat ["\t" ++ show column ++ "\n" | column <- columns] ++ "\nReserve " ++ show reserve
+
+eODeal :: Board
+eODeal = EOBoard [] [[shuffledPack !! (outerIndex * 6 + innerIndex) | innerIndex <- [0..5]] | outerIndex <- [0..7]] [shuffledPack !! index | index <- [48..51]]
+    where
+        shuffledPack = shuffle pack
+
+toFoundations :: Board -> Board
+toFoundations (EOBoard foundations columns reserve) = EOBoard updatedFoundations updatedColumns updatedReserve
+    where
+        (updatedFoundations, updatedColumns, updatedReserve) = doAllPossibleMoves (foundations, columns, reserve)
+
+doAllPossibleMoves :: (Foundations, Columns, Reserve) -> (Foundations, Columns, Reserve)
+doAllPossibleMoves (foundations, columns, reserve) = if null cardsThatCanBeMoved then (foundations, columns, reserve) else
+    doAllPossibleMoves (updatedFoundations, updatedColumns, updatedReserve)
+    where
+        cardsThatCanBeMoved = [head column | column <- columns, isAce (head column) || elem (pCard (head column)) foundations] ++
+            [reserveCard | reserveCard <- reserve, isAce reserveCard || elem (pCard reserveCard) foundations]
+
+        updatedFoundations = updateFoundations (foundations, cardsThatCanBeMoved)
+        updatedColumns = updateColumns (columns, cardsThatCanBeMoved)
+        updatedReserve = updateReserve (reserve, cardsThatCanBeMoved)
+
+updateFoundations :: (Foundations, Deck) -> Foundations
+updateFoundations (foundations, cardsThatCanBeMoved) = cardsThatCanBeMoved ++ [card | card <- foundations, not (elem (sCard card) cardsThatCanBeMoved)]
+
+updateColumns :: (Columns, Deck) -> Columns
+updateColumns (columns, cardsThatCanBeMoved) = [[card | card <- column, not (elem card cardsThatCanBeMoved)] | column <- columns]
+
+updateReserve :: (Reserve, Deck) -> Reserve
+updateReserve (reserve, cardsThatCanBeMoved) = [reserveCard | reserveCard <- reserve, not (elem reserveCard cardsThatCanBeMoved)]
