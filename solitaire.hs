c1d02737ff8c74d97ccf3d541ae2114c11b3742d
Jed G <jed.golebiewski@gmail.com>
2021-11-08 17:14:24 +0000

    Add comments and update some functions

--- solitaire.hs
+++ solitaire.hs
@@ -5,6 +5,7 @@ data Suit = Hearts | Clubs | Spades | Diamonds deriving (Show, Enum, Eq)
 data Pip = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King deriving (Show, Enum, Eq)
 type Card = (Pip, Suit)
 type Deck = [Card]
+
 pack :: Deck
 pack = [(pip, suit) | pip <- enumFrom Ace, suit <- enumFrom Hearts]
 
@@ -20,16 +21,10 @@ isAce (pip, _) = pip == Ace
 isKing :: Card -> Bool
 isKing (pip, _) = pip == King
 
-shuffle :: Deck -> Deck
-shuffle deck = [card | (card, _) <- sortBy cmp (zip deck (randoms (mkStdGen 1) :: [Int]))]
+shuffle :: Int -> Deck -> Deck
+shuffle seed deck = [card | (card, _) <- sortBy cmp (zip deck (randoms (mkStdGen seed) :: [Int]))]
     where
         cmp (_, x) (_, y) = compare x y
-    {-where
-        timeOfDay = timeToTimeOfDay (utctDayTime getCurrentTime)
-        currentHour = todHour timeOfDay
-        currentMin = todMin timeOfDay
-        currentSec = todSec timeOfDay
-        seed = currentHour * 10000 + currentMin * 100 + currentSec-}
 
 type Foundations = [Card]
 type Columns = [[Card]]
@@ -44,7 +39,8 @@ appendixABoard = EOBoard [] [[(Ace, Clubs), (Seven, Diamonds), (Ace, Hearts), (Q
         [(Ace, Spades), (Eight, Clubs), (Ace, Diamonds), (King, Diamonds), (Jack, Hearts), (Four, Clubs)],
         [(Two, Diamonds), (Three, Hearts), (Three, Clubs), (Ten, Hearts), (Six, Diamonds), (Jack, Clubs)],
         [(Nine, Spades), (Four, Diamonds), (Nine, Clubs), (Nine, Hearts), (Three, Spades), (Ten, Spades)],
-        [(Two, Clubs), (Two, Spades), (Four, Hearts), (Nine, Diamonds), (King, Spades), (Eight, Hearts)]] [(Two, Hearts), (Six, Clubs), (Five, Clubs), (Jack, Diamonds)]
+        [(Two, Clubs), (Two, Spades), (Four, Hearts), (Nine, Diamonds), (King, Spades), (Eight, Hearts)]] [(Two, Hearts),
+        (Six, Clubs), (Five, Clubs), (Jack, Diamonds)]
 
 instance Show Board where
     show (EOBoard foundations columns reserve) = "EOBoard\nFoundations " ++ show foundations ++ "\nColumns\n" ++
@@ -54,21 +50,29 @@ instance Show Board where
         concat ["\t[" ++ generateInnerString column outerIndex ++ "]\n" | (column, outerIndex) <- zip columns [1..]] ++
         "Stock " ++ show (length stock `div` 10) ++ " Deals remaining"
         where
+            {- 'hidden' is a list of (Int, Int) tuples, where the first Int is the index of the column where some cards are to be hidden
+            and the second Int is the index in the column, where all the cards with indexes equal or higher are supposed to be hidden.
+
+            A linebreak is placed every 6 cards in a column unless it is the end of the list so that there is nice formatting.-}
+
             generateInnerString col outIndex = 
                 concat [(if null indexOfHiddenDef then show card else if snd(hidden !! (indexOfHiddenDef !! 0)) > innerIndex then show card else "<unknown>") ++
                 (if innerIndex == length col then "" else ",") ++ (if innerIndex `mod` 6 == 0 && innerIndex < length col then "\n\t" else "") |
                 (card, innerIndex) <- zip col [1..], let indexOfHiddenDef = findIndices (\(columnIndex, _) -> columnIndex == outIndex) hidden]
 
-eODeal :: Board
-eODeal = EOBoard [] [[shuffledPack !! (outerIndex * 6 + innerIndex) | innerIndex <- [0..5]] | outerIndex <- [0..7]] [shuffledPack !! index | index <- [48..51]]
+-- 'eODeal' takes in a 'seed' argument on the basis of which it generates a EOBoard.
+eODeal :: Int -> Board
+eODeal seed = EOBoard [] [[shuffledPack !! (outerIndex * 6 + innerIndex) | innerIndex <- [0..5]] | outerIndex <- [0..7]] [shuffledPack !! index | index <- [48..51]]
     where
-        shuffledPack = shuffle pack
+        shuffledPack = shuffle seed pack
 
 toFoundations :: Board -> Board
 toFoundations (EOBoard foundations columns reserve) = EOBoard updatedFoundations updatedColumns updatedReserve
     where
         (updatedFoundations, updatedColumns, updatedReserve) = doAllPossibleMoves (foundations, columns, reserve)
 
+{- Helper function for 'toFoundations'. It recursively calls itself until no cards can be moved. During each call, it creates a list with the cards that can be moved,
+updates 'foundations' with these cards, updates 'columns' and 'reserve' by removing the cards that can be moved. If no cards can be moved, the function terminates.-} 
 doAllPossibleMoves :: (Foundations, Columns, Reserve) -> (Foundations, Columns, Reserve)
 doAllPossibleMoves (foundations, columns, reserve) = if null cardsThatCanBeMoved then (foundations, columns, reserve) else
     doAllPossibleMoves (updatedFoundations, updatedColumns, updatedReserve)
@@ -80,6 +84,7 @@ doAllPossibleMoves (foundations, columns, reserve) = if null cardsThatCanBeMoved
         updatedColumns = updateColumns (columns, cardsThatCanBeMoved)
         updatedReserve = updateReserve (reserve, cardsThatCanBeMoved)
 
+-- Helper functions for 'doAllPossibleMoves'. Functionality explained above.
 updateFoundations :: (Foundations, Deck) -> Foundations
 updateFoundations (foundations, cardsThatCanBeMoved) = cardsThatCanBeMoved ++ [card | card <- foundations, not (elem (sCard card) cardsThatCanBeMoved)]
 
@@ -89,7 +94,9 @@ updateColumns (columns, cardsThatCanBeMoved) = [[card | card <- column, not (ele
 updateReserve :: (Reserve, Deck) -> Reserve
 updateReserve (reserve, cardsThatCanBeMoved) = [reserveCard | reserveCard <- reserve, not (elem reserveCard cardsThatCanBeMoved)]
 
+-- The purpose of this type is explained in the SBoard Show definition above.
 type Hidden = [(Int, Int)]
+
 type Stock = [Card]
 
 columnsInAppendixB :: Columns
@@ -104,20 +111,28 @@ columnsInAppendixB = [[(Eight, Diamonds), (Nine, Hearts)], [(Two, Diamonds)],
     [(Seven, Spades), (Eight, Spades), (Nine, Spades), (Ten, Spades), (Jack, Spades), (Queen, Spades), (King, Spades), (Four, Clubs), (Ten, Clubs), (Ten, Spades)],
     [(Jack, Hearts), (Queen, Hearts)], [(Ace, Clubs), (Two, Clubs)]]
 
-generateSequenceFromTopCard :: Deck -> Card -> Deck
-generateSequenceFromTopCard currentSequence card@(Ace, _) = currentSequence ++ [card]
-generateSequenceFromTopCard currentSequence card = generateSequenceFromTopCard (currentSequence ++ [card]) (pCard card)
-
+{- Function with the purpose of calculating the remaining stock relative to the cards already located in the columns in appendix B board. Makes sure that
+exactly 2 copies of each card are on the playing board since in spider solitaire 2 complete 52 card decks are used.-}
 calculateStockForAppendixB :: Stock
-calculateStockForAppendixB = [card | card <- shuffle pack, iteration <- [0..1], iteration >= countInstances card (columnsInAppendixB ++ foundationsCardsInAppendixB)]
+calculateStockForAppendixB = [card | card <- shuffle seed pack, iteration <- [0..1], iteration >= countInstances card (columnsInAppendixB ++ foundationsCardsInAppendixB)]
     where
         countInstances card columns = foldr (\column outerCount -> foldr (\item innerCount -> if item == card then innerCount+1 else innerCount) outerCount column) 0 columns
         foundationsCardsInAppendixB = [generateSequenceFromTopCard [] (King, Hearts)]
 
+        seed = 1
+
+-- Helper function for 'calculateStockForAppendixB'. Takes in empty list and top card. Returns deck of all the cards of the same suit under the initial card + initial card.
+generateSequenceFromTopCard :: Deck -> Card -> Deck
+generateSequenceFromTopCard currentSequence card@(Ace, _) = currentSequence ++ [card]
+generateSequenceFromTopCard currentSequence card = generateSequenceFromTopCard (currentSequence ++ [card]) (pCard card)
+
 appendixBBoard :: Board
 appendixBBoard = SBoard [(King, Hearts)] columnsInAppendixB [(3, 14), (8, 8)] calculateStockForAppendixB
 
-sDeal :: Board
-sDeal = SBoard [] (concat [[[deck !! (50 + i*6 + j) | j <- [0..5]] | i <- [0..3]], [[deck !! (74 + i*5 + j) | j <- [0..4]] | i <- [0..5]]]) [(i, 2) | i <- [1..10]] (take 50 deck)
+sDeal :: Int -> Board
+sDeal seed = SBoard [] (concat [[[deck !! (50 + i*6 + j) | j <- [0..5]] | i <- [0..3]], [[deck !! (74 + i*5 + j) | j <- [0..4]] | i <- [0..5]]]) [(i, 2) | i <- [1..10]] (take 50 deck)
     where
-        deck = [card | card <- shuffle pack, iteration <- [1..2]]
+        {- New seed is generated for the second shuffle by using the old seed as the generator seed.
+        Without this, the order of the cards in stock could be deduced from the initial position of the cards in the columns.-}
+        (modifiedSeedForSecondShuffle, _) = random (mkStdGen seed)
+        deck = [card | card <- shuffle seed pack] ++ [card | card <- shuffle modifiedSeedForSecondShuffle pack]
